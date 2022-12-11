e973600e48c06f19f66774403653332e8c0dfe7c
Andrew Janong <andrewjanong@gmail.com>
2022-12-11 17:29:33 +0700

    Finished functions but still needs styling

--- main.js
+++ main.js
@@ -19,12 +19,44 @@ const bookTitle = document.getElementById('book-title');
 const bookAuthor = document.getElementById('book-author');
 const bookStatus = document.getElementById('book-status');
 
+function checkDuplicate(title, author) {
+    let duplicate = false;
+
+    for (book of myLibrary) {
+        if (book.title === title && book.author === author) {
+            duplicate = true;
+            break;
+        }
+    }
+
+    return duplicate;
+}
+
+function onlySpaces(str) {
+    return str.trim().length === 0;
+  }
+
+function validInput(title, author) {
+    return !(onlySpaces(title) || onlySpaces(author));
+}
+
 function addBookToLibrary(e) {
 
     // Adding the book to the library
-    const newBook = new Book(bookTitle.value, bookAuthor.value, bookStatus.value);
-    myLibrary.push(newBook);
+    if (validInput(bookTitle.value, bookAuthor.value)) {
+        if (!checkDuplicate(bookTitle.value, bookAuthor.value)) {
+            const newBook = new Book(bookTitle.value.trim(), bookAuthor.value.trim(), bookStatus.value);
+            myLibrary.push(newBook);
+    
+        } else {
+            alert('You have listed a book with the same title and author');
+        }
+    } else {
+        alert('Please enter a valid input');
+    }
 
+    
+    
     // Reset the form
     bookTitle.value = '';
     bookAuthor.value = '';
@@ -32,6 +64,7 @@ function addBookToLibrary(e) {
     // Display book in table
     displayBooks();
     e.preventDefault();
+    
 }
 
 
@@ -44,29 +77,75 @@ function displayBooks() {
 
     for (let book of myLibrary) {
         const newRow = document.createElement('tr');
-        newRow.innerHTML = `
-        <td>${book.title}</td>
-        <td>${book.author}</td>
-        <td><button type="button" class="read">${book.read}</button></td>
-        <td><button type="button" class="remove">Remove</button></td>
-        `
+        if (book.read === 'Read') {
+            newRow.innerHTML = `
+            <td>${book.title}</td>
+            <td>${book.author}</td>
+            <td><button type="button" class="read yes">${book.read}</button></td>
+            <td><button type="button" class="remove">Remove</button></td>
+            `
+        } else  {
+            newRow.innerHTML = `
+            <td>${book.title}</td>
+            <td>${book.author}</td>
+            <td><button type="button" class="read no">${book.read}</button></td>
+            <td><button type="button" class="remove">Remove</button></td>
+            `
+        }
+        
         tableBody.appendChild(newRow);
     }
 
     const removeButtons = document.querySelectorAll("tbody button.remove");
     removeButtons.forEach(button => button.addEventListener('click', removeBook));
 
+    const statusButtons = document.querySelectorAll("tbody button.read");
+    statusButtons.forEach(button => button.addEventListener('click', changeStatus));
+
 }
 
 function removeBook(e) {
     
-    myLibrary = myLibrary.filter(book => book.title !== this.parentElement.parentElement.firstElementChild.textContent);
+    myLibrary = myLibrary.filter(book => (book.title !== this.parentElement.parentElement.firstElementChild.textContent) ||
+                                         (book.author !== this.parentElement.parentElement.getElementsByTagName('td')[1].textContent));
 
     displayBooks();
 }
 
-const removeButtons = document.querySelectorAll("tbody button.remove");
-removeButtons.forEach(button => button.addEventListener('click', removeBook));
+function changeStatus(e) {
+
+    /* IDK WHY THIS DOESN'T WORK 
+    function change(book) {
+        if (book.title === this.parentElement.parentElement.firstElementChild.textContent) {
+            if (book.read === 'Read') {
+                book.read = 'Not read';
+            } else {
+                book.read = 'Read';
+            }
+        }
+
+        return book;
+    }
+
+    myLibrary = myLibrary.map(change);
+    */ 
+
+    for (book of myLibrary) {
+        if (book.title === this.parentElement.parentElement.firstElementChild.textContent) {
+            if (book.read === 'Read') {
+                book.read = 'Not read';
+            } else {
+                book.read = 'Read';
+            }
+        }
+    }
+
+    displayBooks();
+} 
+
+
+
+
 
 
 
